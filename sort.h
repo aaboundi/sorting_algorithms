0060c9a2afbbb14c0f04957ac1e5838525041a8e
aaboundi <aadja@gmail.com>
2022-07-18 09:20:12 -0700

    algo3

--- sort.h
+++ sort.h
@@ -22,7 +22,11 @@ typedef struct listint_s
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
+int len_list(listint_t *h);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void quick_recursion(int *array, int left, int right, size_t size);
+int partition(int *array, int left, int right, size_t size);
 
 #endif
