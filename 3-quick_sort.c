942f88c794135f0e414ec2b21ea688bf14a20020
aaboundi <99649590+aaboundi@users.noreply.github.com>
2022-07-18 16:21:15 +0000

    alogo3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,31 +1,75 @@
 #include "sort.h"
 
 /**
- * selection_sort - sorts an array following the selection sort algorithm
- * @array: array to sort
+ * quick_sort - sorts an array with the Quicksort algorithm
+ * @array: array of ints to sort
  * @size: size of the array
  */
-void selection_sort(int *array, size_t size)
+void quick_sort(int *array, size_t size)
 {
-	unsigned int i, j, min;
-
-	register int tmp;
-
 	if (size < 2)
 		return;
 
-	for (i = 0; i < size; i++)
+	quick_recursion(array, 0, (int)size - 1, size);
+}
+
+/**
+ * quick_recursion - helper function for Quicksort
+ * @array: array to sort
+ * @left: index of the left element
+ * @right: index of the right element
+ * @size: size of the array
+ */
+void quick_recursion(int *array, int left, int right, size_t size)
+{
+	int piv;
+
+	if (left < right)
 	{
-		min = i;
-		for (j = i + 1; j < size; j++)
-		{
-			if  (array[j] < array[min])
-				min = j;
-		}
-		tmp = array[i];
-		array[i] = array[min];
-		array[min] = tmp;
-		if (i != min)
-			print_array(array, size);
+		piv = partition(array, left, right, size);
+		quick_recursion(array, left, piv - 1, size);
+		quick_recursion(array, piv + 1, right, size);
 	}
+}
+
+/**
+ * partition - gives a piv index for Quicksort
+ * @array: array to find the piv in
+ * @left: index of the left element
+ * @right: index of the right element
+ * @size: size of the array
+ *
+ * Return: the index of the piv element
+ */
+int partition(int *array, int left, int right, size_t size)
+{
+	int tmp, i;
+	int j;
+
+	i = left - 1;
+
+	for (j = left; j < right; j++)
+	{
+		if (array[j] < array[right])
+		{
+			i++;
+			if (i != j)
+			{
+				tmp = array[i];
+				array[i] = array[j];
+				array[j] = tmp;
+				print_array(array, size);
+			}
+		}
+	}
+
+	if (array[right] < array[i + 1])
+	{
+		tmp = array[i + 1];
+		array[i + 1] = array[right];
+		array[right] = tmp;
+		print_array(array, size);
+	}
+
+	return (i + 1);
 }
