478ab01816ecb2f9452ec2b9f3752c053ec4ed48
aaboundi <99649590+aaboundi@users.noreply.github.com>
2022-07-19 11:13:51 +0000

    algo100

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,30 +1,35 @@
 #include "sort.h"
-#include <stdio.h>
 
 /**
- * bubble_sort - sorts an array in ascending order
- * @array: array of ints to sort
- * @size: size of the array
- */
-void bubble_sort(int *array, size_t size)
+* shell_sort - function sorts an array of integers using
+* the shell sort algorithm with Knuth sequence
+* @array: the array of integers
+* @size: size of the array
+*
+* Return: none
+*/
+void shell_sort(int *array, size_t size)
 {
-	unsigned int i, j;
+	size_t gap = 0, j, i;
 	int tmp;
 
 	if (size < 2)
 		return;
 
-	for (i = 0; i < size; i++)
+	while ((gap = gap * 3 + 1) < size)
+		;
+
+	gap = (gap - 1) / 3;
+
+	for (; gap > 0; gap = (gap - 1) / 3)
 	{
-		for (j = 0; j < size - i - 1; j++)
+		for (i = gap; i < size; i++)
 		{
-			if  (array[j] > array[j + 1])
-			{
-				tmp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = tmp;
-				print_array(array, size);
-			}
+			tmp = array[i];
+			for (j = i; j >= gap && tmp <= array[j - gap]; j -= gap)
+				array[j] = array[j - gap];
+			array[j] = tmp;
 		}
+		print_array(array, size);
 	}
 }
