bdd1479d135a84ee82e0fbbfa73a8c60627b4022
aaboundi <99649590+aaboundi@users.noreply.github.com>
2022-07-19 14:51:19 +0000

    algo103

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -1,30 +1,78 @@
 #include "sort.h"
-#include <stdio.h>
 
 /**
- * bubble_sort - sorts an array in ascending order
+ * merge_sort - sorts an array with the Merge Sort algorithm
  * @array: array of ints to sort
  * @size: size of the array
  */
-void bubble_sort(int *array, size_t size)
+void merge_sort(int *array, size_t size)
 {
-	unsigned int i, j;
-	int tmp;
+	int *arr;
 
-	if (size < 2)
+	if (!array || size < 2)
 		return;
 
-	for (i = 0; i < size; i++)
+	arr = malloc(sizeof(int) * size);
+
+	merge_recursion(arr, array, 0, size);
+	free(arr);
+}
+
+/**
+ * merge_recursion - recursive function that merge sorts an array
+ * @arr: copy array
+ * @array: array to merge sort
+ * @left: index of the left element
+ * @right: index of the right element
+ */
+void merge_recursion(int *arr, int *array, size_t left, size_t right)
+{
+	size_t middle;
+
+	if (right - left > 1)
 	{
-		for (j = 0; j < size - i - 1; j++)
-		{
-			if  (array[j] > array[j + 1])
-			{
-				tmp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = tmp;
-				print_array(array, size);
-			}
-		}
+		middle = (right - left) / 2 + left;
+		merge_recursion(arr, array, left, middle);
+		merge_recursion(arr, array, middle, right);
+		merge_subarray(arr, array, left, middle, right);
 	}
+}
+
+/**
+ * merge_subarray - merges subarrays
+ * @arr: copy array
+ * @array: array to merge
+ * @left: index of the left element
+ * @middle: index of the middle element
+ * @right: index of the right element
+ */
+void merge_subarray(int *arr, int *array, size_t left,
+		size_t middle, size_t right)
+{
+	size_t i, j, k = 0;
+
+	printf("Merging...\n");
+	printf("[left]: ");
+	print_array(array + left, middle  - left);
+	printf("[right]: ");
+	print_array(array + middle, right - middle);
+
+	for (i = left, j = middle; i < middle && j < right; k++)
+	{
+		if (array[i] < array[j])
+			arr[k] = array[i++];
+		else
+			arr[k] = array[j++];
+	}
+
+	while (i < middle)
+		arr[k++] = array[i++];
+	while (j < right)
+		arr[k++] = array[j++];
+
+	for (k = left, i = 0; k < right; k++)
+		array[k] = arr[i++];
+
+	printf("[Done]: ");
+	print_array(array + left, right - left);
 }
