56634e1273b0b33521051f17bf09dd7fea246303
aaboundi <99649590+aaboundi@users.noreply.github.com>
2022-07-19 15:19:47 +0000

    algo106

--- 106-bitonic_sort.c
+++ 106-bitonic_sort.c
@@ -1,30 +1,75 @@
 #include "sort.h"
-#include <stdio.h>
 
 /**
- * bubble_sort - sorts an array in ascending order
+ * bitonic_sort - sorts an array following the Bitonic sort algorithm
  * @array: array of ints to sort
  * @size: size of the array
  */
-void bubble_sort(int *array, size_t size)
+void bitonic_sort(int *array, size_t size)
 {
-	unsigned int i, j;
-	int tmp;
-
-	if (size < 2)
+	if (!array || size < 2)
 		return;
 
-	for (i = 0; i < size; i++)
+	bitonic_recursion(array, 0, size - 1, 1, size);
+}
+
+/**
+ * bitonic_recursion - recursive function for bitonic sort
+ * @array: array to sort
+ * @l: index of the left-most element
+ * @r: index of the right-most element
+ * @direction: ascending or descending
+ * @size: size of the array
+ */
+void bitonic_recursion(int *array, int l, int r, int direction, size_t size)
+{
+	int step;
+
+	if (r - l >= 1)
 	{
-		for (j = 0; j < size - i - 1; j++)
+		step = (r + l) / 2;
+		printf("Merging [%d/%lu] ", r - l + 1, size);
+		if (direction)
+			printf("(UP):\n");
+		else
+			printf("(DOWN):\n");
+		print_array(array + l, r - l + 1);
+		bitonic_recursion(array, l, step, 1, size);
+		bitonic_recursion(array, step + 1, r, 0, size);
+		bitonic_merge(array, l, r, direction);
+		printf("Result [%d/%lu] ", r - l + 1, size);
+		if (direction)
+			printf("(UP):\n");
+		else
+			printf("(DOWN):\n");
+		print_array(array + l, r - l + 1);
+	}
+}
+
+/**
+ * bitonic_merge - sorts and merges a sequence in ascending or descending order
+ * @array: array to sort
+ * @l: index of the left-most element
+ * @r: index of the right-most element
+ * @direction: ascending or descending
+ */
+void bitonic_merge(int *array, int l, int r, int direction)
+{
+	int tmp, i, step = (l + r) / 2, mid = (r - l + 1) / 2;
+
+	if (r - l >= 1)
+	{
+		for (i = l; i < l + mid; i++)
 		{
-			if  (array[j] > array[j + 1])
+			if (direction == (array[i] > array[i + mid]))
 			{
-				tmp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = tmp;
-				print_array(array, size);
+				tmp = array[i + mid];
+				array[i + mid] = array[i];
+				array[i] = tmp;
 			}
 		}
+		bitonic_merge(array, l, step, direction);
+		bitonic_merge(array, step + 1, r, direction);
 	}
+
 }
