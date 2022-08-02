ef923cd1042e89fbfb81465a63798cb7d33b74af
aaboundi <99649590+aaboundi@users.noreply.github.com>
2022-08-02 13:52:31 +0000

    Update sort.h

--- sort.h
+++ sort.h
@@ -43,6 +43,6 @@ void quick_sort_hoare(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void bitonic_recursion(int *array, int l, int r, int direction, size_t size);
 void bitonic_merge(int *array, int l, int r, int direction);
-void quick_sort_hoare(int *array, size_t size)
+void quick_sort_hoare(int *array, size_t size);
 
 #endif
